50afcd922f0328c7abc512742e0e5a127b204c5b
Charles Nunes <chwn.suporte@gmail.com>
2016-07-05 13:03:04 -0300

    Index.php: Acertos html

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Curso de GIT - Webdev</title>
+</head>
+<body>
+	<?php
 
-define('DS', DIRECTORY_SEPARATOR);
-define('ROOT', dirname(__DIR__));
+		define('DS', DIRECTORY_SEPARATOR);
+		define('ROOT', dirname(__DIR__));
 
-include ROOT.DS.'src'.DS.'core.php';
-include ROOT.DS.'src'.DS.'view.php';
+		include ROOT.DS.'src'.DS.'core.php';
+		include ROOT.DS.'src'.DS.'view.php';
 
-print_r('<pre> DS='.DS.' ROOT='.ROOT.'</pre>');
- 
- ?>
+		echo "<pre> DS=".DS." ROOT=".ROOT."</pre>");
+	 
+	?>
+</body>
+</html>
